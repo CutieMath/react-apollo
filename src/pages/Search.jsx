a9653ab86e338c44766d05de6cf02dc4ba7a23d6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-31 14:48:04 +1000

    use lazy query

---
+++ src/pages/Search.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const GET_CHARACTER_LOCATIONS = gql`
+  query getCharactorLocations($name: String!) {
+    characters(filter: { name: $name }) {
+      results {
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Search = () => {
+  const [name, setName] = useState("");
+  const [getLocations, { loading, error, data, called }] = useLazyQuery(
+    GET_CHARACTER_LOCATIONS,
+    {
+      variables: {
+        name,
+      },
+    }
+  );
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => getLocations()}>Search</button>
+      {loading && <div>Loading</div>}
+      {error && <div>Error</div>}
+      {data && (
+        <ul>
+          {data.characters.results.map((character, index) => (
+            <li key={index}>{character.location.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Search;
